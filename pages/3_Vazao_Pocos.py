ecb3235b5aea260606457be21b83109b3a169afe
JothaP <jotapaulosp@gmail.com>
2026-09-21 18:21:26 -0300

    Update 3_Vazao_Pocos.py

--- pages/3_Vazao_Pocos.py
+++ pages/3_Vazao_Pocos.py
@@ -3,7 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="Vazão de Poços - COI", page_icon="⚙️", layout="wide")
 
 # Verificação segura de sessão e perfil de administrador
-if "autenticado" not in st.session_state or not st.session_state.autenticado:
+from auth import verificar_autenticacao
+
+if not verificar_autenticacao():
     st.warning("Sessão não iniciada ou expirada.")
     if st.button("Ir para o Login"):
         st.switch_page("app.py")
