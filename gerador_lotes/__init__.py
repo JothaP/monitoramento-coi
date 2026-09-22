114c63dc757f35a763749d23f78ba57c16ffd053
JothaP <jotapaulosp@gmail.com>
2026-09-22 15:11:07 -0300

    Update __init__.py

--- gerador_lotes/__init__.py
+++ gerador_lotes/__init__.py
@@ -1,9 +1,29 @@
 from .estado import (
     inicializar_estado,
     obter_base,
-    definir_base,
     base_carregada,
-    limpar_resultado,
+    definir_base,
+    limpar_base,
     limpar_bases,
     voltar_ao_hub,
+    definir_modo_operacao,
+    obter_modo_operacao,
+    obter_base_ativa,
+    existe_base_ativa,
+    ajustar_modo_operacao,
+    limpar_resultado,
+)
+
+from .carregamento import (
+    assinatura_arquivo,
+    assinatura_arquivos,
+    ler_excel,
+    remover_linhas_vazias,
+    consolidar_arquivos,
+    processar_upload_multiplo,
+    processar_upload_unico,
+)
+
+from .exportacao import (
+    dataframe_para_excel,
 )
