b9e0ffe09f862b15357d068c4d14638edff7872f
JothaP <jotapaulosp@gmail.com>
2026-09-22 18:57:15 -0300

    Update __init__.py

--- gerador_lotes/ferramentas/__init__.py
+++ gerador_lotes/ferramentas/__init__.py
@@ -3,3 +3,4 @@ from .duplicidade import render_duplicidade
 from .servicos import render_servicos
 from .eventos import render_eventos
 from .lotes import render_lotes
+from .lista_rapida import render_lista_rapida
