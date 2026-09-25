3f8391cad78bab70302f3ab9009ae180ee26047d
JothaP <jotapaulosp@gmail.com>
2026-09-25 11:42:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-```python
 import streamlit as st
 
 from auth import fazer_login, verificar_autenticacao, fazer_logout
@@ -204,4 +203,3 @@ if st.button(
     st.success("Sessão encerrada com sucesso!")
 
     st.rerun()
-```
