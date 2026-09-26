11cd3ded10b03056dbc6d4b901f8d602b53b4c62
JothaP <jotapaulosp@gmail.com>
2026-09-26 14:54:14 -0300

    Update __init__.py

--- pages/ferramentas_adicionais/__init__.py
+++ pages/ferramentas_adicionais/__init__.py
@@ -2,10 +2,12 @@ from .excel_tools import (
     render_juntar_excel,
     render_visualizar_excel,
     render_comparar_bases,
+    render_remover_duplicidades,
 )
 
 __all__ = [
     "render_juntar_excel",
     "render_visualizar_excel",
     "render_comparar_bases",
+    "render_remover_duplicidades",
 ]
